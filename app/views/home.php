c865b8943c6aba73674f4458bbbb9ef697ae1d47
Joel Vardy <info@joelvardy.com>
2013-11-01 20:04:18 +0000

    Moved static HTML to view

--- app/views/home.php
+++ app/views/home.php
@@ -1,1 +1,23 @@
-Hello World!
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<title>Photo Wall</title>
+		<link rel="stylesheet" href="/assets/css/reset.css" />
+		<link rel="stylesheet" href="/assets/css/design.css" />
+	</head>
+	<body>
+		<div class="grid">
+			<div class="photo square" style="background-image:url('/assets/photos/01.jpg');"></div>
+			<div class="photo wide" style="background-image:url('/assets/photos/02.jpg');"></div>
+			<div class="photo square" style="background-image:url('/assets/photos/03.jpg');"></div>
+			<div class="photo square" style="background-image:url('/assets/photos/04.jpg');"></div>
+			<div class="photo square" style="background-image:url('/assets/photos/05.jpg');"></div>
+			<div class="photo tall" style="background-image:url('/assets/photos/06.jpg');"></div>
+			<div class="photo square" style="background-image:url('/assets/photos/07.jpg');"></div>
+			<div class="photo square" style="background-image:url('/assets/photos/08.jpg');"></div>
+			<div class="photo square" style="background-image:url('/assets/photos/09.jpg');"></div>
+			<div class="photo square" style="background-image:url('/assets/photos/10.jpg');"></div>
+		</div>
+	</body>
+</html>
