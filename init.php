93bac9530ee114e3344302f0241934a0d5a3f07a
Joel Vardy <info@joelvardy.com>
2013-11-01 19:44:37 +0000

    Added routing entry page

---
+++ init.php
@@ -0,0 +1,8 @@
+<?php
+
+// Define error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+// Set PHP timezone
+date_default_timezone_set('Europe/London');
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+// Initialise the application
+require('../init.php');
+
+echo 'Hello World!';
