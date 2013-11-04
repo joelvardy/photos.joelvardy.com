ab644b142d656e4f1b67e2d84dcca16874e3ab23
Joel Vardy <info@joelvardy.com>
2013-11-04 00:58:08 +0000

    Updated meta data

--- app/views/home.php
+++ app/views/home.php
@@ -2,9 +2,11 @@
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-		<title>Photo Wall</title>
+		<title>Photos Taken By Joel Vardy</title>
+		<meta name="description" content="I've taken thousands of photographs, this is a collection of my favourite ones.'" />
 		<link rel="stylesheet" href="/assets/css/reset.css" />
 		<link rel="stylesheet" href="/assets/css/design.css" />
+		<link rel="author" href="https://plus.google.com/102110732747129499789" />
 	</head>
 	<body>
 
