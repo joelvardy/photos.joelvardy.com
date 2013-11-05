5f2f204f3bf757b2325806f93c4501a20a80e7e4
Joel Vardy <info@joelvardy.com>
2013-11-05 21:15:58 +0000

    Asynchronously load scripts

--- app/views/home.php
+++ app/views/home.php
@@ -4,8 +4,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<title>Photos Taken By Joel Vardy</title>
 		<meta name="description" content="I've taken thousands of photographs, this is a collection of my favourite ones.'" />
-		<link async rel="stylesheet" href="/assets/css/reset.css" />
-		<link async rel="stylesheet" href="/assets/css/design.css" />
+		<link rel="stylesheet" href="/assets/css/reset.css" />
+		<link rel="stylesheet" href="/assets/css/design.css" />
 		<link rel="author" href="https://plus.google.com/102110732747129499789" />
 	</head>
 	<body>
