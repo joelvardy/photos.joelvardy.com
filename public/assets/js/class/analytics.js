f9abd112af3cb59fdb60f114e2d79c47aba9f80f
Joel Vardy <info@joelvardy.com>
2013-11-06 00:44:39 +0000

    Moved external link tracking to analytics class

--- public/assets/js/class/analytics.js
+++ public/assets/js/class/analytics.js
@@ -10,6 +10,39 @@ Analytics.prototype = {
 
 	event: function(action, label) {
 		ga('send', 'event', 'userActions', action, label);
+	},
+
+	externalLinks: function() {
+
+		// Track external clicks (only run this if GA has loaded)
+		ga(function() {
+
+			Array.prototype.forEach.call(document.querySelectorAll('p.information a'), function(element) {
+				element.addEventListener('click', function(event) {
+
+					event.preventDefault();
+					var _this = this,
+						eventLabel;
+
+					switch (event.target.classList.toString()) {
+						case 'joelvardy':
+							eventLabel = 'Joel Vardy';
+							break;
+						case 'github':
+							eventLabel = 'GitHub repo';
+							break;
+					}
+
+					ga('send', 'event', 'userActions', 'External link', eventLabel, {
+						'hitCallback': function() {
+							document.location.href = _this.href;
+						}
+					});
+
+				});
+			});
+		});
+
 	}
 
 }
