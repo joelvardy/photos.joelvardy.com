e4e85641892950a9173420e5128e30375cbfc5d9
Joel Vardy <info@joelvardy.com>
2014-10-06 00:15:13 +0100

    Added photo fill directive and styling

--- public/assets/minified/app.js
+++ public/assets/minified/app.js
@@ -259,6 +259,52 @@ photosApp.factory('PhotoData', function () {
 
 });
 
+photosApp.directive('jvPhotoFill', ['$window', function ($window) {
+	return {
+		link: function ($scope, element, attrs) {
+
+			element.css('display', 'none');
+			element.bind('load', function (event) {
+
+				var centre = function () {
+
+					var spacing = 15,
+						windowAspectRatio = ($window.innerWidth - (spacing * 2)) / ($window.innerHeight - (spacing * 2)),
+						photoAspectRatio = element[0].width / element[0].height;
+
+					var photoWidth, photoHeight;
+					if (windowAspectRatio > photoAspectRatio) {
+						photoHeight = ($window.innerHeight - (spacing * 2));
+						photoWidth = Math.floor(photoHeight * photoAspectRatio);
+					} else {
+						photoWidth = ($window.innerWidth - (spacing * 2));
+						photoHeight = Math.floor(photoWidth / photoAspectRatio);
+					}
+
+					element.css({
+						display: 'block',
+						height: photoHeight+'px',
+						left: Math.floor(($window.innerWidth - photoWidth) / 2)+'px',
+						top: Math.floor(($window.innerHeight - photoHeight) / 2)+'px',
+						width: photoWidth+'px'
+					});
+
+				}
+
+				centre();
+
+				angular.element($window).bind('resize', centre);
+
+				$scope.$on('$destroy', function () {
+					angular.element($window).unbind('resize', centre);
+				});
+
+			});
+
+		}
+	};
+}]);
+
 photosApp.controller('GridController', ['$scope', '$http', 'PhotoData', function($scope, $http, PhotoData) {
 
 	if ( ! PhotoData.get().length) {
