c1e593fa99a998f29945e049d18aba5ac6d03652
Joel Vardy <info@joelvardy.com>
2015-04-22 10:28:19 +0100

    Minor refactor / clean up

--- gulpfile.js
+++ gulpfile.js
@@ -2,54 +2,53 @@
 var gulp = require('gulp'),
 	del = require('del'),
 	concat = require('gulp-concat'),
-	notify = require('gulp-notify'),
 	sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css'),
 	uglify = require('gulp-uglifyjs');
 
 
-// SASS
+// Styles
 gulp.task('styles', function () {
-	return gulp.src('public/assets/sass/design.scss')
-	.pipe(sass({
-		style: 'compressed'
-	}))
-	.on('error', notify.onError(function (error) {
-		return error.message;
-	}))
-	.pipe(gulp.dest('public/assets/minified'))
-	.pipe(notify({
-		title: 'Styles',
-		message: 'Task complete'
-	}));
+
+    return sass('./public/assets/scss/design.scss')
+        .pipe(autoprefixer())
+        .pipe(minifyCSS())
+        .on('error', function (error) {
+            console.error('Error!', error);
+        })
+        .pipe(gulp.dest('./public/assets/minified'));
+
 });
 
 
 // JavaScript
 gulp.task('scripts', function () {
-	return gulp.src(['bower_components/angular/angular.min.js', 'bower_components/angular-route/angular-route.min.js', 'bower_components/angular-touch/angular-touch.min.js', 'public/assets/js/**/*.js'])
-	.pipe(concat('app.js'))
-	// .pipe(uglify())
-	.pipe(gulp.dest('public/assets/minified'))
-	.pipe(notify({
-		title: 'Scripts',
-		message: 'Task complete'
-	}));
+
+    return gulp.src(['bower_components/angular/angular.min.js', 'bower_components/angular-route/angular-route.min.js', 'bower_components/angular-touch/angular-touch.min.js', 'public/assets/js/**/*.js'])
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .on('error', function (error) {
+            console.error('Error!', error);
+        })
+        .pipe(gulp.dest('./public/assets/minified'));
+
 });
 
 
 // Clean
-gulp.task('clean', function() {
-	del(['public/assets/minified']);
+gulp.task('clean', function () {
+    del(['./public/assets/minified/*']);
 });
 
 
 // Default task
-gulp.task('default', ['clean'], function() {
-	gulp.start('styles', 'scripts');
+gulp.task('default', ['clean'], function () {
+    gulp.start('styles', 'scripts');
 });
 
 // Watch
-gulp.task('watch', function() {
-	gulp.watch('public/assets/sass/**/*.scss', ['styles']);
-	gulp.watch('public/assets/js/**/*.js', ['scripts']);
+gulp.task('watch', function () {
+    gulp.watch('./public/assets/scss/**/*.scss', ['styles']);
+    gulp.watch('./public/assets/js/**/*.js', ['scripts']);
 });
